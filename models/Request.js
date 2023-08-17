d35dfda06ea5d1ee95439fa5c5e41bb61aed39b6
Abba Danmusa <danmusaabdullahi@gmail.com>
2023-08-17 13:29:46 +0100

    Add request status

--- models/Request.js
+++ models/Request.js
@@ -1,5 +1,96 @@
 const mongoose = require('mongoose')
 
+const authorizationStep = 0
+const approvalStep = 1
+const fileReleaseStep = 2
+const fileReceiveStep = 3
+const statusSchema = new mongoose.Schema({
+  currentStep: {
+    type: Number,
+    enum: [0, 1, 2, 3],
+    default: 0
+  },
+  authorization: {
+    step: {
+      type: Number,
+      enum: [0],
+      default: authorizationStep
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'rejected', 'accepted'],
+      default: 'pending'
+    },
+    authorizedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    dateReceived: {
+      type: Date,
+      default: Date.now()
+    },
+    dateViewed: Date,
+    dateTreated: Date
+  },
+  approval: {
+    step: {
+      type: Number,
+      enum: [1],
+      default: approvalStep
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'rejected', 'accepted'],
+      default: 'pending'
+    },
+    approvedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    dateReceived: Date,
+    dateViewed: Date,
+    dateTreated: Date
+  },
+  fileRelease: {
+    step: {
+      type: Number,
+      enum: [2],
+      default: fileReleaseStep
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'rejected', 'accepted'],
+      default: 'pending'
+    },
+    releasedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    dateReceived: Date,
+    dateViewed: Date,
+    dateTreated: Date
+  },
+  fileReceive: {
+    step: {
+      type: Number,
+      enum: [3],
+      default: fileReceiveStep
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'rejected', 'accepted'],
+      default: 'pending'
+    },
+    receivedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    dateReceived: Date,
+    dateViewed: Date,
+    dateTreated: Date
+  }
+})
+
 const requestSchema = new mongoose.Schema({
   date: {
     type: Date,
@@ -29,7 +120,97 @@ const requestSchema = new mongoose.Schema({
     type: String,
     required: 'Please provide the purpose of the request',
     trim: true
-  }
+  },
+  fileStatus: {
+    isReturned: Boolean,
+    isReceived: Boolean
+  },
+  requestStatus: {
+    currentStep: {
+      type: Number,
+      enum: [0, 1, 2, 3],
+      default: 0
+    },
+    authorization: {
+      step: {
+        type: Number,
+        enum: [0],
+        default: authorizationStep
+      },
+      status: {
+        type: String,
+        enum: ['pending', 'rejected', 'accepted'],
+        default: 'pending'
+      },
+      authorizedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      dateReceived: {
+        type: Date,
+        default: Date.now()
+      },
+      dateViewed: Date,
+      dateTreated: Date
+    },
+    approval: {
+      step: {
+        type: Number,
+        enum: [1],
+        default: approvalStep
+      },
+      status: {
+        type: String,
+        enum: ['pending', 'rejected', 'accepted'],
+        default: 'pending'
+      },
+      approvedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      dateReceived: Date,
+      dateViewed: Date,
+      dateTreated: Date
+    },
+    fileRelease: {
+      step: {
+        type: Number,
+        enum: [2],
+        default: fileReleaseStep
+      },
+      status: {
+        type: String,
+        enum: ['pending', 'rejected', 'accepted'],
+        default: 'pending'
+      },
+      releasedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      dateReceived: Date,
+      dateViewed: Date,
+      dateTreated: Date
+    },
+    fileReceive: {
+      step: {
+        type: Number,
+        enum: [3],
+        default: fileReceiveStep
+      },
+      status: {
+        type: String,
+        enum: ['pending', 'rejected', 'accepted'],
+        default: 'pending'
+      },
+      receivedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      dateReceived: Date,
+      dateViewed: Date,
+      dateTreated: Date
+    }
+  },
 })
 
 mongoose.model('Request', requestSchema)
